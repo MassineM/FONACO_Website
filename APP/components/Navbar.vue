7948a99af5cb0d4349dd8d50813acc3087aec798
Massine <g.fox841@gmail.com>
2022-07-01 18:40:27 +0100

    :rocket: Project init :rocket:

---
+++ APP/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <nav>
+            <ul>
+                <li><div class="navItem"> logo</div></li>
+                <li><div class="navItem"> société</div></li>
+                <li><div class="navItem"> services</div></li>
+                <li><div class="navItem"> équipe</div></li>
+                <li><div class="navItem"> contact</div></li>
+            </ul>
+            <!-- <div>logo</div>
+            <div>société</div>
+            <div>services</div>
+            <div>équipe</div>
+            <div>contact</div> -->
+        </nav>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    
+</style>

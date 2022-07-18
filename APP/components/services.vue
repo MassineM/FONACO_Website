f8f627cd2e1193d1674b18873a4a60f0285c2435
Massine <g.fox841@gmail.com>
2022-07-18 18:14:43 +0100

    :rocket: update #3 18/07/2022

---
+++ APP/components/services.vue
@@ -0,0 +1,24 @@
+<template >
+    <div class="servContainer">
+        <img src="../assets/carouselPosts/123/post_1.png" alt="" class="servImg"/>
+        <div class="servContent">
+            <h1 class="titre1">
+                titre 1
+            </h1>
+            <p class="desc">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia quis magnam vitae rerum dolore reiciendis, reprehenderit ea necessitatibus quia ex deserunt et! Repudiandae eos corporis blanditiis deserunt asperiores adipisci officiis?
+            </p>
+        </div>
+    </div>
+    <div class="servContainer">
+        <div class="servContent"><h1 class="titre1">
+            titre 1
+        </h1>
+        <p class="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia quis magnam vitae rerum dolore reiciendis, reprehenderit ea necessitatibus quia ex deserunt et! Repudiandae eos corporis blanditiis deserunt asperiores adipisci officiis?</p></div><img src="../assets/carouselPosts/123/post_1.png" alt="" class="servImg"/>
+    </div>
+</template>
+<script>
+
+
+</script>
+
